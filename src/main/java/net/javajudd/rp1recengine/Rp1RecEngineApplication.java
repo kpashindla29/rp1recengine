ec8bb5f9f1bbea7d2b5e6ace777416d8c4f470cd
Kishore Pashindla <Kishore.Pashindla@gmail.com>
2024-07-19 15:39:47 +0100

    Created a Spring Boot 2.X app using start.spring.io containing Web dependency.

---
+++ src/main/java/net/javajudd/rp1recengine/Rp1RecEngineApplication.java
@@ -0,0 +1,13 @@
+package net.javajudd.rp1recengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rp1RecEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rp1RecEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/javajudd/rp1recengine/Rp1RecEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javajudd.rp1recengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rp1RecEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
